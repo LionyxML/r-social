eacfa89252276dd8a7009819c181aa381f7479f1
LionyxML <rahul.juliato@gmail.com>
2021-12-29 23:44:14 -0300

    Containers configuration (Docker)

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # r-social
+
+This is a **on-going** project that aims to create a basic social media app (r-social) using the MERN stack (MongoDB, Express, React and NodeJS).
+
+More will be added as the project grows.
+
+# Docker
+
+Each main part of this app runs inside it's own container:
+
+- **react-app** is the client, runs on port 3000
+- **api-server** is the server api, runs on port 5000
+- **mongo** is the database manager, runs on port 27017
+- **mongo-express** is an admin tool for mongodb, runs on port 8081
+
+# Instalation / Usage
+
+Clone this repo, then from the root directory:
+
+Run `make build` to build the docker containers.
+Run `make run` to run the containers with docker-compose.
+Run `make stop` to stop the containers.
--- server/index.js
+++ server/index.js
@@ -2,3 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+const apiPort = 5001;
+
+const connectionString = "mongodb://mongo:27017/r-social";
+mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
+  console.error("MongoDB connection error", e.message);
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(apiPort, () =>
+  console.log(`r-social backend: running on port ${apiPort}`)
+);
