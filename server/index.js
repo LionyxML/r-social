391fc561c32a72ff05332aea0b0ba09e9ab3e2a9
LionyxML <rahul.juliato@gmail.com>
2021-12-30 00:27:35 -0300

    [server] Added post model, created routes for getting and posting posts, added DEV env when running server with npm start

--- server/index.js
+++ server/index.js
@@ -2,27 +2,33 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
+const PORT = process.env.PORT || 5001;
+const APP_MODE = process.env.NODE_ENV;
+const DATABASE_URL =
+  APP_MODE === "DEV"
+    ? "mongodb://localhost:27017/r-social"
+    : "mongodb://mongo:27017/r-social";
 
 const app = express();
-const apiPort = 5001;
 
-const connectionString = "mongodb://mongo:27017/r-social";
-mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
-  console.error("MongoDB connection error", e.message);
-});
+mongoose
+  .connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB is connected"))
+  .catch((e) => {
+    console.error("r-social backend:: MongoDB connection error", e.message);
+  });
 
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
+app.use("/posts", postRoutes);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("This is a backend API for r-social.");
 });
 
-app.listen(apiPort, () =>
-  console.log(`r-social backend: running on port ${apiPort}`)
+app.listen(PORT, () =>
+  console.log(`r-social backend:: api is running on port ${PORT}`)
 );
