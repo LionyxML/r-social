185a3eaa334a3477e5e2b7d589f033402cc7aa50
LionyxML <rahul.juliato@gmail.com>
2021-12-30 15:23:46 -0300

    [server] Like post logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -54,3 +54,21 @@ export const deletePost = async (req, res) => {
 
   res.json({ message: "Post successfully deleted." });
 };
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with the provided id");
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    {
+      likeCount: post.likeCount + 1,
+    },
+    { new: true }
+  );
+
+  res.json(updatedPost);
+};
