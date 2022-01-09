cfb74dcc268b7198e784509454bca19c4f3a41de
LionyxML <rahul.juliato@gmail.com>
2022-01-08 23:45:48 -0300

    Created MUI theme with the default theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 
+const theme = createTheme();
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Container maxWidth="lg">
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/auth" exact element={<Auth />} />
-        </Routes>
-      </Container>
-    </BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Container maxWidth="lg">
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/auth" exact element={<Auth />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </MuiThemeProvider>
   );
 };
 
